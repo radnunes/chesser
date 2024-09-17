5694a319a5f60f348e3cf79ccef8987cb1ce6b9c
Sleeper <sleepervoid.c@gmail.com>
2024-09-17 13:58:27 +0100

    user profile navigation

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -16,10 +16,16 @@ const navigation = [
   { name: 'Calendar', href: '#', current: false },
   { name: 'Reports', href: '#', current: false },
 ]
-const userNavigation = [
+const loggedInUserNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
+]
+
+const loggedOffUserNavigation = [
+  { name: 'Login', href: '/login' },
+  { name: 'Register', href: '/register' },
+  { name: 'FAQ', href: '#' },
 ]
 
 function classNames(...classes) {
@@ -91,7 +97,7 @@ export default function Example() {
                       transition
                       className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                     >
-                      {userNavigation.map((item) => (
+                      {loggedOffUserNavigation.map((item) => (
                         <MenuItem key={item.name}>
                           <a
                             href={item.href}
@@ -153,7 +159,7 @@ export default function Example() {
                 </button>
               </div>
               <div className="mt-3 space-y-1 px-2">
-                {userNavigation.map((item) => (
+                {loggedOffUserNavigation.map((item) => (
                   <DisclosureButton
                     key={item.name}
                     as="a"
